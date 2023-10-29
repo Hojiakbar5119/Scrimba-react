77557a3c04f25e3b0ea5700bccf512a07e061bb6
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-10-29 15:35:59 +0500

    scrimba create header

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
+import Hello from './component/header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     <h1>Hello world</h1>
     <em>by Scrimba online platform</em>
+    <Hello/>
   </React.StrictMode>,
 )

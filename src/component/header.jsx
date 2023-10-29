1aa190c6fd7222923119541ec181d7134faec130
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-10-29 19:45:14 +0500

    import image

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import './header.css'
+import logo from './img/Vite.png'
 
 class Hello extends React.Component{
 
     render(){
         return(
-            <h1>Header</h1>
+            <div>
+                <img src={logo} alt="" />
+                <h1>My awesome website in React</h1>
+                <h1>I love React</h1>
+                <ol>
+                    <li>It's composable</li>
+                    <li>It's declarative</li>
+                    <li>It's a heriable skills</li>
+                    <li>It's a heriable skills</li>
+                </ol>
+            </div>
         )
     }
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Hello from './component/header'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <h1>Hello world</h1>
-    <em>by Scrimba online platform</em>
     <Hello/>
   </React.StrictMode>,
 )

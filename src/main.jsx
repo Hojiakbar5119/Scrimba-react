77384ade9c7d27762967099c1700b5e8ec12c331
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-11-02 21:39:16 +0500

    wirte commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from './App.jsx'
 import "./index.css";
-import Content from "./component/content";
-import Navbar from "./component/navbar";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <Navbar />
-    <Content />
+    {/* <Navbar />
+    <Content /> */}
+    <h1>Creat Airbnb</h1>
   </React.StrictMode>
 );
